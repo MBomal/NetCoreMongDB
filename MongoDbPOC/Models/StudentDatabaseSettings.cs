dc26fabc84d0fe1a260672daf3c0549cc796ab04
Mel <Mel@DESKTOP-BHFST2D>
2019-10-16 19:03:02 +0200

    Cleanup

--- MongoDbPOC/Models/StudentDatabaseSettings.cs
+++ MongoDbPOC/Models/StudentDatabaseSettings.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace MongoDbPOC.Models
+﻿namespace MongoDbPOC.Models
 {
 	public class StudentDatabaseSettings : IStudentDatabaseSettings
 		{
